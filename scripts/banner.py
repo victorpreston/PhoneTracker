c435323d1e0b8acc3fb1b1acbec71dc23a7d9af2
victorpreston <prestonvictor25@gmail.com>
2023-07-10 02:02:10 +0300

    Update Code

--- scripts/banner.py
+++ scripts/banner.py
@@ -26,7 +26,7 @@ def banner():
     print(c.ran + logo)
 
     print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, "[+] Linkedin: Victor Preston ", "- " * 4 + c.ran + "|")
-    print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, "[+] Follow me on Instagram @vpreston254 ", "- " * 4+c.ran + "|")
+    print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, "[+] Follow me on Twitter @vpreston254 ", "- " * 4+c.ran + "|")
     print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/victorpreston ", "- " * 3+c.ran + "|")
 
 
